cc6623e765f48efeb546b30712e5096ee99106ea
Samuel Silva Valencia <sv807227@gmail.com>
2023-12-26 21:54:20 -0300

    quarta versão - Usando BufferedWrite

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,34 +1,26 @@
 package aplication;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		// Implementação de forma totalmente manual!
+		String[] lines = new String[] {"Good morning", "Goog afternoon", "Good night"};
+
 		String path = "D:\\temp\\in.txt";
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			
-
-			/*
-			 * OBS: Pego a stream básica e a partir dela eu instâncio a stream que tem o
-			 * esquema de buffer que deixa mais rápida a leitura.
-			 */
-
-			String line = br.readLine(); // Lê apenas uma linha do arquivo.
-
-			while (line != null) {
-				System.out.println(line); //Se a linha não for nula,imprima.
-				line = br.readLine(); //continue lendo as linas...
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
 		}
-
+		catch(IOException e) {
+			System.out.println("Error: "+e.getMessage());
+		}
 	}
 
 }
