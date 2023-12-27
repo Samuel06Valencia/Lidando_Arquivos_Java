83e4fb59776d5b80acc14377037aa095b3ca15dc
Samuel Silva Valencia <sv807227@gmail.com>
2023-12-26 21:17:01 -0300

    Versão final - Bloco try-with-resources

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -10,15 +10,9 @@ public class Program {
 
 		// Implementação de forma totalmente manual!
 		String path = "D:\\temp\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
-
-		try {
-			fr = new FileReader(path); // sequencia de leitura(Stream) a partir do arquivo que estiver nesse caminho
-			br = new BufferedReader(fr); // O BufferedReader é instanciado a partir do FileReader, ela é uma abstração
-											// maior.
-
-			// Versão resumida:br = new BufferedReader(new FileReader(path));
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
 
 			/*
 			 * OBS: Pego a stream básica e a partir dela eu instâncio a stream que tem o
@@ -28,22 +22,11 @@ public class Program {
 			String line = br.readLine(); // Lê apenas uma linha do arquivo.
 
 			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
+				System.out.println(line); //Se a linha não for nula,imprima.
+				line = br.readLine(); //continue lendo as linas...
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 
 	}
